aa73185aa810f60ac3ac0066bf04ed57250f8bd1
mariolarios <108195141+mariolarios@users.noreply.github.com>
2022-10-13 10:39:05 -0400

    answers for lesson 1

---
+++ 01-node-tutorial/answers/content/06_alternative_flavor.js
@@ -0,0 +1,8 @@
+// This is one way to do it
+module.exports.items = ["item1", "item2"];
+
+const person = {
+    name: "bob", 
+}
+// This is another way to do it
+module.exports.singlePerson = person
---
+++ 01-node-tutorial/answers/content/09_path_module.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+console.log(path.sep);
+
+const filePath = path.join("answers", "content", "subfolder", "test.txt");
+
+console.log(filePath);
+
+const base = path.basename(filePath);
+console.log(base);
+
+const absolute = path.resolve(
+  __dirname,
+  "answers",
+  "content",
+  "subfolder",
+  "test.txt"
+);
+console.log(absolute);
