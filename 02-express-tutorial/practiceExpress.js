245b0c9474243f094d59803ec2afcc033abc5f82
mariolarios <108195141+mariolarios@users.noreply.github.com>
2022-10-29 11:20:40 -0400

    answers for lesson 3

---
+++ 02-express-tutorial/practiceExpress.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./new-public/index.html"));
+});
+
+app.get("/sample", (req, res) => {
+  res.send("This is working");
+});
+
+app.listen(3000, () => {
+  console.log("server is listening on port 3000....");
+});
+
